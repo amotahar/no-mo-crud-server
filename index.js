8d276820576ff7f885549790cc4d774ae94d80d1
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-04-13 10:55:52 +0600

    middleware

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json())
+
 
 app.get('/', (req, res, next) => {
   res.send('Hello CRUD!');
