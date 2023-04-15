cb1075cf146e7f0fecc3d752d273a166b2af7fed
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-04-15 18:07:27 +0600

    put delete

--- index.js
+++ index.js
@@ -46,6 +46,31 @@ async function run(){
       const result = await userCollection.insertOne(user);
       res.send(result);
     })
+
+    app.put('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const user = req.body;
+      const option = {upsert: true};
+      const updatedUser = {
+          $set: {
+              name: user.name,
+              address: user.address,
+              email: user.email
+          }
+      }
+      const result = await userCollection.updateOne(filter, updatedUser, option);
+      res.send(result);
+  })
+
+  app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      // console.log('trying to delete', id);
+      const query = { _id: ObjectId(id) }
+      const result = await userCollection.deleteOne(query);
+      console.log(result);
+      res.send(result);
+  });
    
   }
   
